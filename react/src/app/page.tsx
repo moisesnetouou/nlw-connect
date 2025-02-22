9d3efc54e6f85f45bebc66443801a80474f0dd2d
Moisés <moisesnetored@gmail.com>
2025-02-22 18:09:51 -0400

    feat: add input component

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -1,14 +1,15 @@
-import {ArrowRight, Copy} from "lucide-react"
+import {ArrowRight, Copy, Mail} from "lucide-react"
 
 import { Button } from "@/components/button";
 import { IconButton } from "../components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
     <main>
 
 
-      <Button>
+      <Button type="submit">
           NLW
 
           <ArrowRight />        
@@ -17,6 +18,16 @@ export default function Home() {
       <IconButton>
         <Copy />        
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField placeholder="E-mail" />
+        </InputRoot>
+      </div>
     </main>
   );
 }
