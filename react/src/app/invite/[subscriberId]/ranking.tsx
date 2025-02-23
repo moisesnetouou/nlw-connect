b3840e6d145458399221ae3a4dc7e052efdca0f2
Moisés <moisesnetored@gmail.com>
2025-02-22 22:50:11 -0400

    feat: requests and render data from api

--- react/src/app/invite/[subscriberId]/ranking.tsx
+++ react/src/app/invite/[subscriberId]/ranking.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 
+import { getRanking } from '@/http/api'
 import cooper from '../../../assets/medal-cooper.svg'
 import gold from '../../../assets/medal-gold.svg'
 import silver from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full md:max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -12,41 +15,49 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="rounded-xl relative bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Moisés Neto
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <div
+              key={item.id}
+              className="rounded-xl relative bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {item.name}
+              </span>
 
-          <Image src={gold} alt="medal" className="absolute top-0 right-8" />
-        </div>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-        <div className="rounded-xl relative bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Moisés Neto
-          </span>
+              {rankingPosition === 1 && (
+                <Image
+                  src={gold}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+              {rankingPosition === 2 && (
+                <Image
+                  src={silver}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <Image src={silver} alt="medal" className="absolute top-0 right-8" />
-        </div>
-
-        <div className="rounded-xl relative bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Moisés Neto
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image src={cooper} alt="medal" className="absolute top-0 right-8" />
-        </div>
+              {rankingPosition === 3 && (
+                <Image
+                  src={cooper}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
