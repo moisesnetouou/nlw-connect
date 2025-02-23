4c4383158bf5bfc880a22f80443870a1f3d63369
Moisés <moisesnetored@gmail.com>
2025-02-22 20:59:10 -0400

    feat: add invite page, componentization and organization of the project

--- react/src/components/icon-button.tsx
+++ react/src/components/icon-button.tsx
@@ -1,11 +1,14 @@
 import type { ComponentProps } from 'react'
-
+import { twMerge } from 'tailwind-merge'
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer hover:bg-blue hover:text-gray-500 transition-colors duration-300"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer hover:bg-blue hover:text-gray-500 transition-colors duration-300',
+        className
+      )}
       {...props}
     />
   )
