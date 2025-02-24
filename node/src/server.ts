a563f2cb91e6d2b6ac744969f03af0c472a483d2
Moisés <moisesnetored@gmail.com>
2025-02-23 20:55:15 -0400

    feat: add validation to .env files

--- node/src/server.ts
+++ node/src/server.ts
@@ -9,6 +9,7 @@ import {
 import {fastifySwagger} from "@fastify/swagger"
 import {fastifySwaggerUi} from "@fastify/swagger-ui"
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -31,11 +32,10 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs',
 })
 
-
 app.register(subscribeToEventRoute)
 
 app.listen({
-  port: 3333
+  port: env.PORT
 }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
 })
