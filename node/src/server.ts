9d50c64d76ff87ff191216a213494d3e2f4508d2
Moisés <moisesnetored@gmail.com>
2025-02-23 20:44:15 -0400

    feat: add documentation with swagger

--- node/src/server.ts
+++ node/src/server.ts
@@ -3,9 +3,12 @@ import { fastifyCors } from "@fastify/cors"
 import { 
   validatorCompiler, 
   serializerCompiler, 
-  type ZodTypeProvider
+  type ZodTypeProvider,
+  jsonSchemaTransform
 } from "fastify-type-provider-zod"
-import {z} from 'zod'
+import {fastifySwagger} from "@fastify/swagger"
+import {fastifySwaggerUi} from "@fastify/swagger-ui"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,27 +17,22 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.post('/subscriptions', {
-  schema: {
-    body: z.object({
-      name: z.string(),
-      email: z.string().email()
-    }),
-    response: {
-      201: z.object({
-        name: z.string(),
-        email: z.string().email()
-      })
-    }
-  }
-}, async (request, reply) => {
-  const {name, email} = request.body
-
-  return reply.status(201).send({
-    name,
-    email
-  })
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1'
+    },
+  },
+  transform: jsonSchemaTransform
 })
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+
+app.register(subscribeToEventRoute)
 
 app.listen({
   port: 3333
